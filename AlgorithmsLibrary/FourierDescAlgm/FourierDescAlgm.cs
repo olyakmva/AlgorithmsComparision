df2fc05bdad2e21be7676621950b589a88e3a043
Timofei Teterin <timofei.teterin@inbox.ru>
2022-03-10 12:50:56 +0300

    Add LineSegmentClass & ShapeAnalysysClass

---
+++ AlgorithmsLibrary/FourierDescAlgm/FourierDescAlgm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLibrary.FourierDescAlgm
+{
+    public class FourierDescAlgm : ISimplificationAlgm
+    {
+        public SimplificationAlgmParameters Options { get; set; }
+
+        public virtual void Run(MapData map)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Run(ref List<MapPoint> chain, int startIndex, int endIndex)
+        {
+            
+        }
+    }
+}
---
+++ AlgorithmsLibrary/FourierDescAlgm/LineSegmentClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLibrary.FourierDescAlgm 
+{
+    /// <summary>
+    /// Фрагмент полилинии
+    /// </summary>
+    public class LineSegment
+    {
+        public int Id; //Порядковый номер
+        public double StartPos; //Начальная позиция
+        public double EndPos; //Конечная позиция
+        public double Orient; //Азимут
+    }
+}
